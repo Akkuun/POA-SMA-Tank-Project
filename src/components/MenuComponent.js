658ed750c6d8da540540d299b44ee74293649041
Mathis <90316879+Akkuun@users.noreply.github.com>
2024-09-19 13:59:46 +0200

    Add menu component

---
+++ src/components/MenuComponent.js
@@ -0,0 +1,14 @@
+const MenuComponent = () => {
+    return (
+        <div>
+            <h1>Menu</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MenuComponent;
