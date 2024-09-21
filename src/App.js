f7f9a1a8af781717e84bf4e38584afb5923b444d
unknown <mathis.duban@etu.umontpellier.fr>
2024-09-21 12:07:57 +0200

    game started when Start is clicked

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import MenuComponent from "./components/MenuComponent";
 import SettingsComponent from "./components/SettingsComponent";
+import MainPage from "./components/MainPage";
 
 const App = () => {
     // settings Array ==> 0: tankNumber, 1: isPlayerPlaying
@@ -18,6 +19,7 @@ const App = () => {
         <Routes>
             <Route path="/" element={<MenuComponent settings={settings} />} />
             <Route path="/settings" element={<SettingsComponent onSettingsChange={updateSettings} />} />
+            <Route  path="/game" element={<MainPage settings={settings} />} />
         </Routes>
     );
 };
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -8,10 +8,14 @@ const MenuComponent = ({ settings }) => {
         navigate('/settings');
     };
 
+    const handleClickStart = () => {
+        navigate('/game');
+    }
+
     return (
         <div id="Menu">
             <h1>Menu</h1>
-            <button>Start</button>
+            <button onClick={handleClickStart}>Start</button>
             <button onClick={handleClickOption}>Options</button>
             <h1>Tank Number: {settings[0]}</h1> {/* Nombre de tanks */}
             <h1>Is Player Playing: {settings[1] ? 'Yes' : 'No'}</h1> {/* Booléen converti en texte */}
