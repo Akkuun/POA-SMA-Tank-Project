2b07729d319c3accc2555c664f9801c96f941c2f
Akkuun <mathisduban@gmail.com>
2024-10-11 15:57:40 +0200

    style menu improved + go back navigate added

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,8 +1,10 @@
 import '../styles/MenuComponent.css';
 import { useNavigate } from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
+import BackgroundImage from '../assets/Background.jpg';
 
 const MenuComponent = ({ settings }) => {
-    const navigate = useNavigate(); // Initialiser useNavigate
+    const navigate = useNavigate();
 
     const handleClickOption = () => {
         navigate('/settings');
@@ -10,15 +12,39 @@ const MenuComponent = ({ settings }) => {
 
     const handleClickStart = () => {
         navigate('/game');
-    }
+    };
 
     return (
         <div id="Menu">
-            <h1>Menu</h1>
-            <button onClick={handleClickStart}>Start</button>
-            <button onClick={handleClickOption}>Options</button>
-            <h1>Tank Number: {settings[0]}</h1> {/* Nombre de tanks */}
-            <h1>Is Player Playing: {settings[1] ? 'Yes' : 'No'}</h1> {/* Booléen converti en texte */}
+            <div id="LeftPane">
+                {/* Ajouter une image de fond ou une animation ici */}
+            </div>
+            <div id="Bloc">
+                <TypeAnimation
+                    sequence={['Welcome to the Tank Game!', 1000, 'Prepare for Battle!', 1000]}
+                    wrapper="h1"
+                    cursor={true}
+                    repeat={Infinity}
+                    className="animated-title"
+                />
+                <button onClick={handleClickStart}>Start</button>
+                <button onClick={handleClickOption}>Options</button>
+
+                {/* Utilisation de TypeAnimation pour les settings avec styles identiques aux boutons */}
+                <TypeAnimation
+                    sequence={[
+                        'Tank Number: ' + settings[0],
+                        5000,
+                        '',
+                        5000,
+                        'Is Player Playing: ' + (settings[1] ? 'Yes' : 'No'),
+                    ]}
+                    wrapper="div"
+                    cursor={true}
+                    repeat={Infinity}
+                    className="settings-info"
+                />
+            </div>
         </div>
     );
 };
